0c143abd1b6c802b653395f77dadcfd46ff353d0
Trond Trosterud <trond.trosterud@uit.no>
2020-12-11 16:01:51 +0100

    Remove in.txt after run

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -29,6 +29,7 @@ do
 #  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-mpj/src/generator-gt-norm.xfst
   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-mpj/src/generator-gt-norm.hfstol
  done
+ rm -f $L_FILE
 done
 
 
